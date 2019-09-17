06c7cb431c3c4f464d035986012f91ce995bdd2b
Walker Trent <walktrent@gmail.com>
2019-09-16 22:13:01 -0500

    Changed post controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,2 @@
 class Post < ApplicationRecord
-    def index
-    end
-
-    def new
-        @post = Post.new
-    end
-
-    def create
-        @post = Post.new(post_params)
-    end
-
-private
-
-    def post_params
-        params.require(:post).permit(:title, :content)
-    end
 end
